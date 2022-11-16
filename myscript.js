973b67b06e99780d922192ffbc704a32e72c9aeb
Hanne Roeland <hanne.roeland@itvitaelearning.nl>
2022-11-16 13:50:27 +0100

    v3

---
+++ myscript.js
@@ -0,0 +1,8 @@
+var band = "Rammstein";
+        const band2 = {type:"math metal", name:"Meshuggah", seenLive:0};
+        band2.seenLive = 1;
+
+function myFunction() {
+    document.getElementById("p1").innerHTML = "Sonne";
+    document.getElementById("p2").innerHTML = band;
+}
