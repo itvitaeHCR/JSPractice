00a75a58e49cb177765066566949e1c6cd38ff58
Hanne Roeland <hanne.roeland@itvitaelearning.nl>
2022-11-16 14:13:27 +0100

    v4

--- myscript.js
+++ myscript.js
@@ -6,3 +6,13 @@ function myFunction() {
     document.getElementById("p1").innerHTML = "Sonne";
     document.getElementById("p2").innerHTML = band;
 }
+
+function askForName() {
+    var name = prompt("Please state your name: ", "no name provided");
+    document.getElementById("p3").innerHTML = name;
+}
+
+function getConfirmation() {
+    var yesNo = confirm("I'm assuming you love this website?");
+    document.getElementById("p4").innerHTML = yesNo
+}
